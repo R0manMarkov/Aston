9b32cae7a142b032e96fedc2196c5722ab4a60fd
Misterlis <ramses.and1@gmail.com>
2025-01-24 02:22:32 +0300

    Задание 11

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Исходный массив
-        int[] array = {1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+        // Создание пустого массива длиной 100
+        int[] array = new int[100];
 
-        // Вывод исходного массива
-        System.out.println("Исходный массив: " + Arrays.toString(array));
+        // Заполнение массива числами от 1 до 100
+        fillArray(array);
 
-        // Замена 0 на 1 и 1 на 0
-        invertArray(array);
-
-        // Вывод изменённого массива
-        System.out.println("Изменённый массив: " + Arrays.toString(array));
+        // Вывод заполненного массива
+        System.out.println("Заполненный массив: " + Arrays.toString(array));
     }
 
-    /**
-     * Метод для инвертирования элементов массива (0 -> 1, 1 -> 0)
-     * @param array Массив, элементы которого нужно инвертировать
-     */
-    public static void invertArray(int[] array) {
+    public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            // Если элемент равен 0, заменить на 1, иначе заменить на 0
-            array[i] = (array[i] == 0) ? 1 : 0;
+            array[i] = i + 1; // Записываем в массив число (индекс + 1)
         }
     }
 }

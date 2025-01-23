fc5d868a06e21e9dcc69d4c18980299f9eee3225
Misterlis <ramses.and1@gmail.com>
2025-01-24 01:40:49 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        printThreeWords();
+        checkSumSign();
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static void checkSumSign() {
+        int a = 5;  // Пример значения переменной a
+        int b = -10; // Пример значения переменной b
+
+        int sum = a + b; // Суммируем переменные
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }

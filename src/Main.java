a5ad2eaf2e35f3ea67d8283d4abefea6595c9719
Misterlis <ramses.and1@gmail.com>
2025-01-28 12:09:16 +0300

    Урок 4

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,123 @@
 import java.util.Arrays;
 
-
-
 public class Main {
     public static void main(String[] args) {
-        // Пример вызова метода
-        int len = 10;
-        int initialValue = 5;
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        System.out.println("Сумма в диапазоне: " + isSumInRange(5, 10));
 
-        // Создание массива
-        int[] resultArray = createArray(len, initialValue);
+        printNumberSign(10);
+        printNumberSign(-5);
 
-        // Вывод массива на экран
-        System.out.println("Результирующий массив: " + Arrays.toString(resultArray));
+        System.out.println("Число -5 отрицательное: " + isNegative(-5));
+
+        printStringMultipleTimes("Привет, мир!", 5);
+
+        int year = 2024;
+        System.out.println("Год " + year + (isLeapYear(year) ? " високосный" : " не високосный"));
+
+        int[] array1 = {1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+        System.out.println("Исходный массив: " + Arrays.toString(array1));
+        invertArray(array1);
+        System.out.println("Изменённый массив: " + Arrays.toString(array1));
+
+        int[] array2 = new int[100];
+        fillArray(array2);
+        System.out.println("Заполненный массив: " + Arrays.toString(array2));
+
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Исходный массив: " + Arrays.toString(array3));
+        modifyArray(array3);
+        System.out.println("Изменённый массив: " + Arrays.toString(array3));
     }
 
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 
-    public static int[] createArray(int len, int initialValue) {
-        // Создаём массив заданной длины
-        int[] array = new int[len];
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -10;
+        int sum = a + b;
 
-        // Заполняем массив значением initialValue
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
         }
+    }
 
-        return array; // Возвращаем заполненный массив
+    public static void printColor() {
+        int value = 50;
+
+        if (value < 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 20;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static boolean isSumInRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    public static void printNumberSign(int number) {
+        if (number >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    public static void printStringMultipleTimes(String str, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(str);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void invertArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] == 0) ? 1 : 0;
+        }
+    }
+
+    public static void fillArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+    }
+
+    public static void modifyArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
     }
 }
-
-

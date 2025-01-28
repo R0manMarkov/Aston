f9b118946b020928e1c47b6f716624db5b2796a1
Misterlis <ramses.and1@gmail.com>
2025-01-28 13:41:33 +0300

    Урок 5

---
+++ src/Employee.java
@@ -0,0 +1,30 @@
+// Employee.java
+public class Employee {
+    private String fullName;  // ФИО
+    private String position;  // Должность
+    private String email;     // Email
+    private String phone;     // Телефон
+    private double salary;    // Зарплата
+    private int age;          // Возраст
+
+    // Конструктор
+    public Employee(String fullName, String position, String email, String phone, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    // Метод для вывода информации об объекте
+    public void printInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+        System.out.println();
+    }
+}
